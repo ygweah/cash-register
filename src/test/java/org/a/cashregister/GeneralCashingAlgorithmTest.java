186a2496816d3d63014c4787b2173cd43bb1e38f
Yuhong Guo <yuhong@rocketmiles.com>
2016-06-28 17:56:16 -0500

    add more edge case tests

--- src/test/java/org/a/cashregister/GeneralCashingAlgorithmTest.java
+++ src/test/java/org/a/cashregister/GeneralCashingAlgorithmTest.java
@@ -7,71 +7,88 @@ import java.math.BigDecimal;
 
 public class GeneralCashingAlgorithmTest {
 
-    private CashingAlgorithm cashingAlgorithm = new GeneralCashingAlgorithm();
+	private CashingAlgorithm cashingAlgorithm = new GeneralCashingAlgorithm();
 
-    @Test
-    public void testSingleDenomination() {
-        Cash cash = new Cash();
-        cash.add(new CashValue(new BigDecimal("17.00"), 1));
-        cash.add(new CashValue(new BigDecimal("13.00"), 1));
-        cash.add(new CashValue(new BigDecimal("11.00"), 1));
+	@Test
+	public void testSingleDenomination() {
+		Cash cash = new Cash();
+		cash.add(new CashValue(new BigDecimal("17.00"), 1));
+		cash.add(new CashValue(new BigDecimal("13.00"), 1));
+		cash.add(new CashValue(new BigDecimal("11.00"), 1));
 
-        BigDecimal withdrawnAmount = new BigDecimal("13.00");
-        Cash withdrawnCash = cashingAlgorithm.withdraw(cash, withdrawnAmount);
+		BigDecimal withdrawnAmount = new BigDecimal("13.00");
+		Cash withdrawnCash = cashingAlgorithm.withdraw(cash, withdrawnAmount);
 
-        Assert.assertNotNull(withdrawnCash);
-        Assert.assertEquals(withdrawnCash.getTotalAmount(), withdrawnAmount);
-        Assert.assertEquals(withdrawnCash.getCashValue(new BigDecimal("13.00")).getCount(), 1);
-    }
+		Assert.assertNotNull(withdrawnCash);
+		Assert.assertEquals(withdrawnCash.getTotalAmount(), withdrawnAmount);
+		Assert.assertEquals(withdrawnCash.getCashValue(new BigDecimal("13.00")).getCount(), 1);
+	}
 
-    @Test
-    public void testSuccess() {
-        Cash cash = new Cash();
-        cash.add(new CashValue(new BigDecimal("20.00"), 2));
-        cash.add(new CashValue(new BigDecimal("5.00"), 2));
-        cash.add(new CashValue(new BigDecimal("2.00"), 2));
-        cash.add(new CashValue(new BigDecimal("1.00"), 2));
+	@Test
+	public void testSuccess() {
+		Cash cash = new Cash();
+		cash.add(new CashValue(new BigDecimal("20.00"), 2));
+		cash.add(new CashValue(new BigDecimal("5.00"), 2));
+		cash.add(new CashValue(new BigDecimal("2.00"), 2));
+		cash.add(new CashValue(new BigDecimal("1.00"), 2));
 
-        BigDecimal withdrawnAmount = new BigDecimal("28.00");
-        Cash withdrawnCash = cashingAlgorithm.withdraw(cash, withdrawnAmount);
+		BigDecimal withdrawnAmount = new BigDecimal("28.00");
+		Cash withdrawnCash = cashingAlgorithm.withdraw(cash, withdrawnAmount);
 
-        Assert.assertNotNull(withdrawnCash);
-        Assert.assertEquals(withdrawnCash.getTotalAmount(), withdrawnAmount);
-        Assert.assertEquals(withdrawnCash.getCashValue(new BigDecimal("20.00")).getCount(), 1);
-        Assert.assertEquals(withdrawnCash.getCashValue(new BigDecimal("5.00")).getCount(), 1);
-        Assert.assertEquals(withdrawnCash.getCashValue(new BigDecimal("2.00")).getCount(), 1);
-        Assert.assertEquals(withdrawnCash.getCashValue(new BigDecimal("1.00")).getCount(), 1);
-    }
+		Assert.assertNotNull(withdrawnCash);
+		Assert.assertEquals(withdrawnCash.getTotalAmount(), withdrawnAmount);
+		Assert.assertEquals(withdrawnCash.getCashValue(new BigDecimal("20.00")).getCount(), 1);
+		Assert.assertEquals(withdrawnCash.getCashValue(new BigDecimal("5.00")).getCount(), 1);
+		Assert.assertEquals(withdrawnCash.getCashValue(new BigDecimal("2.00")).getCount(), 1);
+		Assert.assertEquals(withdrawnCash.getCashValue(new BigDecimal("1.00")).getCount(), 1);
+	}
 
-    @Test
-    public void testInsufficientLargeBill() {
-        Cash cash = new Cash();
-        cash.add(new CashValue(new BigDecimal("2.00"), 1));
-        cash.add(new CashValue(new BigDecimal("1.00"), 5));
+	@Test
+	public void testInsufficientLargeBill() {
+		Cash cash = new Cash();
+		cash.add(new CashValue(new BigDecimal("2.00"), 1));
+		cash.add(new CashValue(new BigDecimal("1.00"), 5));
 
-        BigDecimal withdrawnAmount = new BigDecimal("5.00");
-        Cash withdrawnCash = cashingAlgorithm.withdraw(cash, withdrawnAmount);
+		BigDecimal withdrawnAmount = new BigDecimal("5.00");
+		Cash withdrawnCash = cashingAlgorithm.withdraw(cash, withdrawnAmount);
 
-        Assert.assertNotNull(withdrawnCash);
-        Assert.assertEquals(withdrawnCash.getTotalAmount(), withdrawnAmount);
-        Assert.assertEquals(withdrawnCash.getCashValue(new BigDecimal("2.00")).getCount(), 1);
-        Assert.assertEquals(withdrawnCash.getCashValue(new BigDecimal("1.00")).getCount(), 3);
-    }
+		Assert.assertNotNull(withdrawnCash);
+		Assert.assertEquals(withdrawnCash.getTotalAmount(), withdrawnAmount);
+		Assert.assertEquals(withdrawnCash.getCashValue(new BigDecimal("2.00")).getCount(), 1);
+		Assert.assertEquals(withdrawnCash.getCashValue(new BigDecimal("1.00")).getCount(), 3);
+	}
 
-    @Test
-    public void testTwoDollarEdgeCase() {
-        Cash cash = new Cash();
-        cash.add(new CashValue(new BigDecimal("20.00"), 2));
-        cash.add(new CashValue(new BigDecimal("5.00"), 2));
-        cash.add(new CashValue(new BigDecimal("2.00"), 5));
+	@Test
+	public void testTwoDollarEdgeCase() {
+		Cash cash = new Cash();
+		cash.add(new CashValue(new BigDecimal("20.00"), 2));
+		cash.add(new CashValue(new BigDecimal("5.00"), 2));
+		cash.add(new CashValue(new BigDecimal("2.00"), 5));
 
-        BigDecimal withdrawnAmount = new BigDecimal("28.00");
-        Cash withdrawnCash = cashingAlgorithm.withdraw(cash, withdrawnAmount);
+		BigDecimal withdrawnAmount = new BigDecimal("28.00");
+		Cash withdrawnCash = cashingAlgorithm.withdraw(cash, withdrawnAmount);
 
-        Assert.assertNotNull(withdrawnCash);
-        Assert.assertEquals(withdrawnCash.getTotalAmount(), withdrawnAmount);
-        Assert.assertEquals(withdrawnCash.getCashValue(new BigDecimal("20.00")).getCount(), 1);
-        Assert.assertEquals(withdrawnCash.getCashValue(new BigDecimal("2.00")).getCount(), 4);
-    }
+		Assert.assertNotNull(withdrawnCash);
+		Assert.assertEquals(withdrawnCash.getTotalAmount(), withdrawnAmount);
+		Assert.assertEquals(withdrawnCash.getCashValue(new BigDecimal("20.00")).getCount(), 1);
+		Assert.assertEquals(withdrawnCash.getCashValue(new BigDecimal("2.00")).getCount(), 4);
+	}
+
+	@Test
+	public void testTwoDollarEdgeCase2() {
+		Cash cash = new Cash();
+		cash.add(new CashValue(new BigDecimal("20.00"), 2));
+		cash.add(new CashValue(new BigDecimal("5.00"), 2));
+		cash.add(new CashValue(new BigDecimal("2.00"), 5));
+
+		BigDecimal withdrawnAmount = new BigDecimal("31.00");
+		Cash withdrawnCash = cashingAlgorithm.withdraw(cash, withdrawnAmount);
+
+		Assert.assertNotNull(withdrawnCash);
+		Assert.assertEquals(withdrawnCash.getTotalAmount(), withdrawnAmount);
+		Assert.assertEquals(withdrawnCash.getCashValue(new BigDecimal("20.00")).getCount(), 1);
+		Assert.assertEquals(withdrawnCash.getCashValue(new BigDecimal("5.00")).getCount(), 1);
+		Assert.assertEquals(withdrawnCash.getCashValue(new BigDecimal("2.00")).getCount(), 3);
+	}
 
 }
--- src/test/java/org/a/cashregister/UsCashingAlgorithmTest.java
+++ src/test/java/org/a/cashregister/UsCashingAlgorithmTest.java
@@ -7,71 +7,88 @@ import java.math.BigDecimal;
 
 public class UsCashingAlgorithmTest {
 
-    private CashingAlgorithm cashingAlgorithm = new UsCashingAlgorithm();
+	private CashingAlgorithm cashingAlgorithm = new UsCashingAlgorithm();
 
-    @Test
-    public void testSingleDenomination() {
-        Cash cash = new Cash();
-        cash.add(UsDenomination.FIVE_DOLLAR.toCashValue(2));
-        cash.add(UsDenomination.TWO_DOLLAR.toCashValue(2));
-        cash.add(UsDenomination.ONE_DOLLAR.toCashValue(2));
+	@Test
+	public void testSingleDenomination() {
+		Cash cash = new Cash();
+		cash.add(UsDenomination.FIVE_DOLLAR.toCashValue(2));
+		cash.add(UsDenomination.TWO_DOLLAR.toCashValue(2));
+		cash.add(UsDenomination.ONE_DOLLAR.toCashValue(2));
 
-        BigDecimal withdrawnAmount = new BigDecimal("5.00");
-        Cash withdrawnCash = cashingAlgorithm.withdraw(cash, withdrawnAmount);
+		BigDecimal withdrawnAmount = new BigDecimal("5.00");
+		Cash withdrawnCash = cashingAlgorithm.withdraw(cash, withdrawnAmount);
 
-        Assert.assertNotNull(withdrawnCash);
-        Assert.assertEquals(withdrawnCash.getTotalAmount(), withdrawnAmount);
-        Assert.assertEquals(withdrawnCash.getCashValue(UsDenomination.FIVE_DOLLAR.getValue()).getCount(), 1);
-    }
+		Assert.assertNotNull(withdrawnCash);
+		Assert.assertEquals(withdrawnCash.getTotalAmount(), withdrawnAmount);
+		Assert.assertEquals(withdrawnCash.getCashValue(UsDenomination.FIVE_DOLLAR.getValue()).getCount(), 1);
+	}
 
-    @Test
-    public void testSuccess() {
-        Cash cash = new Cash();
-        cash.add(UsDenomination.TWENTY_DOLLAR.toCashValue(2));
-        cash.add(UsDenomination.FIVE_DOLLAR.toCashValue(2));
-        cash.add(UsDenomination.TWO_DOLLAR.toCashValue(2));
-        cash.add(UsDenomination.ONE_DOLLAR.toCashValue(2));
+	@Test
+	public void testSuccess() {
+		Cash cash = new Cash();
+		cash.add(UsDenomination.TWENTY_DOLLAR.toCashValue(2));
+		cash.add(UsDenomination.FIVE_DOLLAR.toCashValue(2));
+		cash.add(UsDenomination.TWO_DOLLAR.toCashValue(2));
+		cash.add(UsDenomination.ONE_DOLLAR.toCashValue(2));
 
-        BigDecimal withdrawnAmount = new BigDecimal("28.00");
-        Cash withdrawnCash = cashingAlgorithm.withdraw(cash, withdrawnAmount);
+		BigDecimal withdrawnAmount = new BigDecimal("28.00");
+		Cash withdrawnCash = cashingAlgorithm.withdraw(cash, withdrawnAmount);
 
-        Assert.assertNotNull(withdrawnCash);
-        Assert.assertEquals(withdrawnCash.getTotalAmount(), withdrawnAmount);
-        Assert.assertEquals(withdrawnCash.getCashValue(UsDenomination.TWENTY_DOLLAR.getValue()).getCount(), 1);
-        Assert.assertEquals(withdrawnCash.getCashValue(UsDenomination.FIVE_DOLLAR.getValue()).getCount(), 1);
-        Assert.assertEquals(withdrawnCash.getCashValue(UsDenomination.TWO_DOLLAR.getValue()).getCount(), 1);
-        Assert.assertEquals(withdrawnCash.getCashValue(UsDenomination.ONE_DOLLAR.getValue()).getCount(), 1);
-    }
+		Assert.assertNotNull(withdrawnCash);
+		Assert.assertEquals(withdrawnCash.getTotalAmount(), withdrawnAmount);
+		Assert.assertEquals(withdrawnCash.getCashValue(UsDenomination.TWENTY_DOLLAR.getValue()).getCount(), 1);
+		Assert.assertEquals(withdrawnCash.getCashValue(UsDenomination.FIVE_DOLLAR.getValue()).getCount(), 1);
+		Assert.assertEquals(withdrawnCash.getCashValue(UsDenomination.TWO_DOLLAR.getValue()).getCount(), 1);
+		Assert.assertEquals(withdrawnCash.getCashValue(UsDenomination.ONE_DOLLAR.getValue()).getCount(), 1);
+	}
 
-    @Test
-    public void testInsufficientLargeBill() {
-        Cash cash = new Cash();
-        cash.add(UsDenomination.TWO_DOLLAR.toCashValue(1));
-        cash.add(UsDenomination.ONE_DOLLAR.toCashValue(5));
+	@Test
+	public void testInsufficientLargeBill() {
+		Cash cash = new Cash();
+		cash.add(UsDenomination.TWO_DOLLAR.toCashValue(1));
+		cash.add(UsDenomination.ONE_DOLLAR.toCashValue(5));
 
-        BigDecimal withdrawnAmount = new BigDecimal("5.00");
-        Cash withdrawnCash = cashingAlgorithm.withdraw(cash, withdrawnAmount);
+		BigDecimal withdrawnAmount = new BigDecimal("5.00");
+		Cash withdrawnCash = cashingAlgorithm.withdraw(cash, withdrawnAmount);
 
-        Assert.assertNotNull(withdrawnCash);
-        Assert.assertEquals(withdrawnCash.getTotalAmount(), withdrawnAmount);
-        Assert.assertEquals(withdrawnCash.getCashValue(UsDenomination.TWO_DOLLAR.getValue()).getCount(), 1);
-        Assert.assertEquals(withdrawnCash.getCashValue(UsDenomination.ONE_DOLLAR.getValue()).getCount(), 3);
-    }
+		Assert.assertNotNull(withdrawnCash);
+		Assert.assertEquals(withdrawnCash.getTotalAmount(), withdrawnAmount);
+		Assert.assertEquals(withdrawnCash.getCashValue(UsDenomination.TWO_DOLLAR.getValue()).getCount(), 1);
+		Assert.assertEquals(withdrawnCash.getCashValue(UsDenomination.ONE_DOLLAR.getValue()).getCount(), 3);
+	}
 
-    @Test
-    public void testTwoDollarEdgeCase() {
-        Cash cash = new Cash();
-        cash.add(UsDenomination.TWENTY_DOLLAR.toCashValue(2));
-        cash.add(UsDenomination.FIVE_DOLLAR.toCashValue(2));
-        cash.add(UsDenomination.TWO_DOLLAR.toCashValue(5));
+	@Test
+	public void testTwoDollarEdgeCase() {
+		Cash cash = new Cash();
+		cash.add(UsDenomination.TWENTY_DOLLAR.toCashValue(2));
+		cash.add(UsDenomination.FIVE_DOLLAR.toCashValue(2));
+		cash.add(UsDenomination.TWO_DOLLAR.toCashValue(5));
 
-        BigDecimal withdrawnAmount = new BigDecimal("28.00");
-        Cash withdrawnCash = cashingAlgorithm.withdraw(cash, withdrawnAmount);
+		BigDecimal withdrawnAmount = new BigDecimal("28.00");
+		Cash withdrawnCash = cashingAlgorithm.withdraw(cash, withdrawnAmount);
 
-        Assert.assertNotNull(withdrawnCash);
-        Assert.assertEquals(withdrawnCash.getTotalAmount(), withdrawnAmount);
-        Assert.assertEquals(withdrawnCash.getCashValue(UsDenomination.TWENTY_DOLLAR.getValue()).getCount(), 1);
-        Assert.assertEquals(withdrawnCash.getCashValue(UsDenomination.TWO_DOLLAR.getValue()).getCount(), 4);
-    }
+		Assert.assertNotNull(withdrawnCash);
+		Assert.assertEquals(withdrawnCash.getTotalAmount(), withdrawnAmount);
+		Assert.assertEquals(withdrawnCash.getCashValue(UsDenomination.TWENTY_DOLLAR.getValue()).getCount(), 1);
+		Assert.assertEquals(withdrawnCash.getCashValue(UsDenomination.TWO_DOLLAR.getValue()).getCount(), 4);
+	}
+
+	@Test
+	public void testTwoDollarEdgeCase2() {
+		Cash cash = new Cash();
+		cash.add(UsDenomination.TWENTY_DOLLAR.toCashValue(2));
+		cash.add(UsDenomination.FIVE_DOLLAR.toCashValue(2));
+		cash.add(UsDenomination.TWO_DOLLAR.toCashValue(5));
+
+		BigDecimal withdrawnAmount = new BigDecimal("31.00");
+		Cash withdrawnCash = cashingAlgorithm.withdraw(cash, withdrawnAmount);
+
+		Assert.assertNotNull(withdrawnCash);
+		Assert.assertEquals(withdrawnCash.getTotalAmount(), withdrawnAmount);
+		Assert.assertEquals(withdrawnCash.getCashValue(UsDenomination.TWENTY_DOLLAR.getValue()).getCount(), 1);
+		Assert.assertEquals(withdrawnCash.getCashValue(UsDenomination.FIVE_DOLLAR.getValue()).getCount(), 1);
+		Assert.assertEquals(withdrawnCash.getCashValue(UsDenomination.TWO_DOLLAR.getValue()).getCount(), 3);
+	}
 
 }
